2c7086b0f1165dd89c4435e1f08048dbe40e4b9e
Yadavabhishek123 <abhishekyadav20je0043@gmail.com>
2024-05-30 17:41:17 +0530

    Commit file changes

---
+++ Class_Java.java
@@ -0,0 +1,14 @@
+public class Class_Java{
+
+    int x = 5;
+    String s = "Abhishek Yadab";
+    public static void main(String[]argu){
+        // creating a object using new keyword
+        // syntax is :- ClassName obj_name = ClassName()
+
+        Class_Java myobj = new Class_Java(); // myobj is a object
+
+        // Invoking this object
+        System.out.println(myobj.x); // vakue of this is 5, this line access x
+    }
+}
