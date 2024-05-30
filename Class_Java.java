bc63e242de4c7d894f71519d8511951be3c66da3
Yadavabhishek123 <abhishekyadav20je0043@gmail.com>
2024-05-30 20:56:19 +0530

    Commit file changes

--- Class_Java.java
+++ Class_Java.java
@@ -7,6 +7,10 @@ public class Class_Java{
         // syntax is :- ClassName obj_name = ClassName()
 
         Class_Java myobj = new Class_Java(); // myobj is a object
+        Class_Method obj = new Class_Method(); // i can create object into this calss by another class
+        // and also use the method of another class like multiply
+        int mul = Class_Method.multiply(5, 6);
+        System.out.println(mul);
 
         // Invoking this object
         System.out.println(myobj.x); // vakue of this is 5, this line access x
--- Class_Method.java
+++ Class_Method.java
@@ -1,3 +1,46 @@
 public class Class_Method {
+    // Creating class method
+    // function that are created inside a class are called class methods
+    // example
 
+    static int return_keyword(int a, int b){
+        return a+b;
+    }
+
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+
+    public int subtract(int a, int b){ // public method without static keyword
+        return a-b;
+    }
+
+    static void mymethod(int age, String name){ // name of this method is mymethod
+        // write logic here
+        System.out.println("Hello java I am Abhishek yadav my age is " + age);
+    }
+    // parameters are  any variable that we are adding inside parenthesis are called parameters
+    // here age and name are parameters
+
+
+    public static void main(String[] argu){ // this is a public static main method
+
+        mymethod(21, "Abhishek Yadav");
+        int a = 2;
+        int b = 20;
+        int sum = return_keyword(a, b);
+        System.out.println(sum);
+
+        // calling a static method here
+
+        // here we not access this static method, so we access by class name
+        int res = Class_Method.multiply(a, b);
+        System.out.println(res);
+
+        // calling a public method
+        // we can access this ethod by creating a object, see below
+        Class_Method myobj = new Class_Method();
+        int ans = myobj.subtract(a, b);
+        System.out.println(ans);
+    }
 }
